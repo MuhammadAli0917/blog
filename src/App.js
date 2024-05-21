1604b91119da41f24e801c60beb0b0ad7d059c60
alideveloper <92316845+MuhammadAli0917@users.noreply.github.com>
2024-05-20 23:44:02 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./index.css"
+function App() {
+  return (
+      <div className="App">
+        <div className="header">
+          <div className="left">
+            <h1>thetalaba's blog</h1>
+          </div>
+          <div className="right">
+            <h3>Blog</h3>
+            <h3>Galareya</h3>
+            <h3>Haqimda</h3>
+          </div>
+        </div>
+        <h3>Github</h3>
+      </div>
+  );
+}
+
+export default App;
